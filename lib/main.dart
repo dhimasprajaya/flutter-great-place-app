852b521a381d0b0d2b0d2123bd8eaf40726f2f48
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-19 23:49:30 +0700

    using image picker and device camera

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider.value(
       value: GreatPlaces(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Great Places App',
         theme: ThemeData(
           primarySwatch: Colors.indigo,
