a7fd26a49574a500ef93481a767521aba2dd2fcd
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-20 05:46:56 +0700

    storing location in sql lite

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,11 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import '../config.dart';
+
+const apiKey = Config.GOOGLE_API_KEY;
 
 class LocationHelper {
   static String generateLocationPreviewImage({
     double latitude,
     double longitude,
   }) {
-    final apiKey = Config.GOOGLE_API_KEY;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:S%7C$latitude,$longitude&key=$apiKey';
+    // return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:S%7C$latitude,$longitude&key=$apiKey';
+    return 'https://jurnalapps.co.id/assets/img/content/1502783940_1502169523_1.jpg';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    // final url =
+    //     'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$apiKey';
+    // final response = await http.get(url);
+    // return json.decode(response.body)['results'][0]['formatted_address'];
+    return 'Dummy Address';
   }
 }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -6,6 +6,10 @@ import '../helpers/location_helper.dart';
 import '../screens/map_screen.dart';
 
 class LocationInput extends StatefulWidget {
+  final Function onSelectPlace;
+
+  LocationInput(this.onSelectPlace);
+
   @override
   _LocationInputState createState() => _LocationInputState();
 }
@@ -13,16 +17,26 @@ class LocationInput extends StatefulWidget {
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
 
-  Future<void> _getCurrentUserLocation() async {
-    final locData = await Location().getLocation();
+  void _showPreview(double lat, double lng) {
     final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
-      latitude: locData.latitude,
-      longitude: locData.longitude,
+      latitude: lat,
+      longitude: lng,
     );
 
     setState(() {
       _previewImageUrl = staticMapImageUrl;
     });
+  }
+
+  Future<void> _getCurrentUserLocation() async {
+    try {
+      final locData = await Location().getLocation();
+      _showPreview(locData.latitude, locData.longitude);
+      widget.onSelectPlace(locData.latitude, locData.longitude);
+    } catch (error) {
+      print('error get current location');
+      return;
+    }
   }
 
   Future<void> _selectOnMap() async {
@@ -39,7 +53,8 @@ class _LocationInputState extends State<LocationInput> {
       return;
     }
 
-    print(selectedLocation.latitude);
+    _showPreview(selectedLocation.latitude, selectedLocation.longitude);
+    widget.onSelectPlace(selectedLocation.latitude, selectedLocation.longitude);
   }
 
   @override
